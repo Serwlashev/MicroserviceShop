595010a70576350aba5fad323e3a463c9ff7f39e
Сергей Шевченко <Serwlashev@gmail.com>
2021-11-01 10:11:41 +0200

    Implemented Domain and Application layers for Ordering service

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpate = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpate is null)
+            {
+                _logger.LogError("Order not exist in database");
+                throw NotFoundException(nameof(Order), request.Id);
+            }
+
+            _mapper.Map(request, orderToUpate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpate);
+
+            _logger.LogInformation($"Order {orderToUpate.Id} is successfully updated.");
+
+            return Unit.Value;
+        }
+
+        private Exception NotFoundException(string v, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/OrderListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class OrderListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public OrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserName(request.UserName);
+
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
